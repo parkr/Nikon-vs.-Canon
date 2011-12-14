04b160bc5b5424835e0ebbed2ccd31bc8e55f274
Parker Moore <parkrmoore@gmail.com>
2011-12-13 22:59:49 -0500

    now looping to get pages... increases randomness

--- get.rb
+++ get.rb
@@ -1,73 +1,82 @@
 #!/usr/bin/env ruby -wKU
 
-require 'flickraw'
-require 'helpers'
+require 'flickraw-cached'
+require "#{File.dirname(__FILE__)}/helpers"
 
-SEPARATOR = "Nikon
+SEPARATOR = "\nNikon
 ------------------------------------------------------------
-Canon"
+Canon\n"
 
 FlickRaw.api_key = "88d95a26510271b373cadcdd9e18ab14"
 FlickRaw.shared_secret = "8e86dfa5ebf15ef8"
 
 dump = File.open("cameras.txt", "a+")
 
-
+options = {
+  :tags => nil,
+  :page => 1,
+  :per_page => 10,
+  :privacy_filter => 1,
+  :media => "photos",
+  :accuracy => 3,
+  #:machine_tags => "camera:",
+  :content_type => 1
+}
+pages = [5,4,3,2,1]
 
 begin
-  nikon = flickr.photos.search( :tags => "nikon",
-                                :privacy_filter => 1,
-                                :per_page => 500, 
-                                :media => "photos",
-                                :accuracy => 1,
-                                #:machine_tags => "camera:",
-                                :content_type => 1)
-                            
-  good_nikon = File.open("good_nikon.csv", "a+")
-  nikon.each do |p|
-    begin
-      exif = flickr.photos.getExif(:photo_id => p.id)
-      dump.write("#{p.id} taken with a #{exif.camera}\n")
-      if exif.camera.include?("Nikon")
-        url = FlickRaw.url_b(flickr.photos.getInfo(:photo_id => p.id))
-        puts "(#{p.id}) #{p.title}: #{url}"
-        if exif.camera.include?("Nikon") && exif.camera.index(/D(\d+)(S?)/) != nil
-          good_nikon.write "#{p.id},\"Nikon\",\"#{url}\"\n"
+  
+  options[:tags] = "nikon"
+  pages.each do |page|
+    options[:page] = page
+    nikon = flickr.photos.search( options )
+                          
+    good_nikon = File.open("good_nikon.csv", "a+")
+    nikon.each do |p|
+      begin
+        exif = flickr.photos.getExif(:photo_id => p.id)
+        dump.write("#{p.id} taken with a #{exif.camera}\n")
+        if exif.camera.include?("Nikon")
+          url = FlickRaw.url_b(flickr.photos.getInfo(:photo_id => p.id))
+          puts "(#{p.id}) #{p.title}: #{url}"
+          if exif.camera.include?("Nikon") && exif.camera.index(/D(\d+)(S?)/) != nil
+            good_nikon.write "#{p.id},\"Nikon\",\"#{url}\"\n"
+          end
         end
+      rescue FlickRaw::FailedResponse
+      rescue Timeout::Error, Errno::ETIMEDOUT
+      rescue Interrupt
+        exit(0)
       end
-    rescue FlickRaw::FailedResponse
-    rescue Timeout::Error, Errno::ETIMEDOUT
-    rescue Interrupt
-      exit(0)
     end
+    nikon = nil
   end
 
   puts SEPARATOR
  
-  canon = flickr.photos.search( :tags => "canon", 
-                                :privacy_filter => 1,
-                                :per_page => 500, 
-                                :media => "photos", 
-                                :accuracy => 1,
-                                #:machine_tags => "camera:", 
-                                :content_type => 1)
+ 
+  options[:tags] = "canon"
+  pages.each do |page|
+    options[:page] = page
+    canon = flickr.photos.search( options )
                                 
-  good_canon = File.open("good_canon.csv", "a+")
-  canon.each do |p|
-    begin
-      exif = flickr.photos.getExif(:photo_id => p.id)
-      dump.write("#{p.id} taken with a #{exif.camera}\n")
-      if exif.camera.include?("Canon")
-        url = FlickRaw.url_b(flickr.photos.getInfo(:photo_id => p.id))
-        puts "(#{p.id}) #{p.title}: #{url}"
-        if exif.camera.include?("Canon") && Helpers.include?(exif.camera, ["EOS", "Rebel", "Mark"])
-          good_canon.write "#{p.id},\"Canon\",\"#{url}\"\n"
+    good_canon = File.open("good_canon.csv", "a+")
+    canon.each do |p|
+      begin
+        exif = flickr.photos.getExif(:photo_id => p.id)
+        dump.write("#{p.id} taken with a #{exif.camera}\n")
+        if exif.camera.include?("Canon")
+          url = FlickRaw.url_b(flickr.photos.getInfo(:photo_id => p.id))
+          puts "(#{p.id}) #{p.title}: #{url}"
+          if exif.camera.include?("Canon") && Helpers.include?(exif.camera, ["EOS", "Rebel", "Mark"])
+            good_canon.write "#{p.id},\"Canon\",\"#{url}\"\n"
+          end
         end
+      rescue FlickRaw::FailedResponse
+      rescue Timeout::Error, Errno::ETIMEDOUT
+      rescue Interrupt
+        exit(0)
       end
-    rescue FlickRaw::FailedResponse
-    rescue Timeout::Error, Errno::ETIMEDOUT
-    rescue Interrupt
-      exit(0)
     end
   end
   
